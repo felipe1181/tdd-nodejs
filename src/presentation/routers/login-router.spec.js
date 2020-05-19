59e6233eed401496fbd366d28a012be6db0fca6d
felipe1181 <felipe1181jua@gmail.com>
2020-05-19 10:17:28 -0300

    test: criar teste existencia do email na requisição em login router

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,18 @@
-describe('Name of the group', () => {
-  test('should ', () => {
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.email) return { statusCode: 400 }
+  }
+}
 
+describe('Login router', () => {
+  test('retornar erro 400 se email não existir', () => {
+    const sut = new LoginRouter() // System under test
+    const httpRequest = {
+      body: {
+        senha: 'minha_senha'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
   })
 })
