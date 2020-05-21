8b1eb8af5a93eaf0142c8226007992fa348d115b
felipe1181 <felipe1181jua@gmail.com>
2020-05-21 11:33:58 -0300

    refactor: realizar condicional para aceitar somente email válido

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,15 +1,17 @@
 const httpResponse = require('../helpers/http-response')
 
 module.exports = class LoginRouter {
-  constructor (authUseCase) {
+  constructor (authUseCase, emailValidator) {
     this.authUseCase = authUseCase
+    this.emailValidator = emailValidator
   }
 
   async route (httpRequest) {
     try {
       const { email, senha } = httpRequest.body
-      if (!email) return httpResponse.badRequest('email')
-      if (!senha) return httpResponse.badRequest('senha')
+      if (!email) return httpResponse.badRequestMissing('email')
+      if (!this.emailValidator.isValid(email)) return httpResponse.badRequestInvalid('email')
+      if (!senha) return httpResponse.badRequestMissing('senha')
 
       const tokenDeAcesso = await this.authUseCase.auth(email, senha)
       if (!tokenDeAcesso) return httpResponse.unauthorizedError()
